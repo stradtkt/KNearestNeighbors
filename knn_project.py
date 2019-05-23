b9746f10d77acf9126a3574061540f3791498359
Kevin <stradtkt22@gmail.com>
2019-05-23 17:39:49 -0400

    Finished knn_project

--- knn_project.py
+++ knn_project.py
@@ -26,3 +26,25 @@ from sklearn.model_selection import train_test_split
 X = df_feat
 y = df['TARGET CLASS']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train,y_train)
+pred = knn.predict(X_test)
+
+from sklearn.metrics import classification_report, confusion_matrix
+print(classification_report(y_test, pred))
+print(confusion_matrix(y_test, pred))
+
+error_rate = []
+for i in range(1, 40):
+    knn = KNeighborsClassifier(n_neighbors=1)
+    knn.fit(X_train, y_train)
+    pred_i = knn.predict(X_test)
+    error_rate.append(np.mean(pred_i != y_test))
+    
+plt.figure(figsize=(10,6))
+plt.plot(range(1,40), error_rate, color='blue', linestyle='dashed', marker='o', markerfacecolor='red', markersize=10)
+plt.title('Error Rate vs K Value')
+plt.xlabel('K')
+plt.ylabel('Error Rate')
