ce36fcbf4a884f0391687cccc6efb0bb867e5ad9
Kevin <stradtkt22@gmail.com>
2019-05-23 17:24:18 -0400

    Added to knn

--- knn.py
+++ knn.py
@@ -33,3 +33,16 @@ pred = knn.predict(X_test)
 from sklearn.metrics import classification_report, confusion_matrix
 print(classification_report(y_test, pred))
 print(confusion_matrix(y_test, pred))
+
+error_rate = []
+for i in range(1, 40):
+    knn = KNeighborsClassifier(n_neighbors=1)
+    knn.fit(X_train, y_train)
+    pred_i = knn.predict(X_test)
+    error_rate.append(np.mean(pred_i != y_test))
+    
+plt.figure(figsize=(10,6))
+plt.plot(range(1,40), error_rate, color='blue', linestyle='dashed', marker='o', markerfacecolor='red', markersize=10)
+plt.title('Error Rate vs K Value')
+plt.xlabel('K')
+plt.ylabel('Error Rate')
