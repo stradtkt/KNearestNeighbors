edcb1cf7d66fd1daa7395eaafa47b180662d537c
Kevin <stradtkt22@gmail.com>
2019-05-23 15:49:17 -0400

    Added to knn

--- knn.py
+++ knn.py
@@ -4,4 +4,32 @@ Created on Thu May 23 15:32:48 2019
 
 @author: Kevin
 """
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+df = pd.read_csv('Classified Data',index_col=0)
+df.head()
+
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(df.drop('TARGET CLASS', axis=1))
+scaled_features = scaler.transform(df.drop('TARGET CLASS', axis=1))
+scaled_features
+df_feat = pd.DataFrame(scaled_features, columns=df.columns[:-1])
+df_feat.head()
+
+from sklearn.model_selection import train_test_split
+X = df_feat
+y = df['TARGET CLASS']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=101)
+
+from sklearn.neighbors import KNeighborsClassifier
+knn = KNeighborsClassifier(n_neighbors=1)
+knn.fit(X_train,y_train)
+pred = knn.predict(X_test)
+
+from sklearn.metrics import classification_report, confusion_matrix
+print(classification_report(y_test, pred))
+print(confusion_matrix(y_test, pred))
